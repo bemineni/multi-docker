3da2fc86eb57a851ee71379ba8f67c8636511472
Srikanth Bemineni <srikanth.bemineni@gmail.com>
2019-12-24 13:57:56 -0600

    server logs

--- server/index.js
+++ server/index.js
@@ -42,7 +42,8 @@ app.get('/', (req, res) => {
 
 app.get('/values/all', async (req, res) => {
     const values = await pgClient.query('SELECT * from values');
-
+    console.log("recieved values from postgres")
+    console.log(values.rows)
     res.send(values.rows);
 });
 
